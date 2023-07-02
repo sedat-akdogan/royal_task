3b56d90b1d74ce00cedcce4a99a2bf225f66f44a
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-07-02 21:26:43 +0100

    allcomponents work fine, no data displayed!

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DataComponent
     ],
     imports: [
         BrowserModule, 
